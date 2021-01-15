4905c7bb9e599667094f165b1b8cbc79089c659c
jj2020driver <jj2020driver@gmail.com>
2021-01-15 23:05:21 +0200

    Test commit

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import { BrowserRouter as Router } from "react-router-dom";
 import { ContextProvider } from "./context";
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
 	<ContextProvider>
@@ -12,8 +11,3 @@ ReactDOM.render(
 	</ContextProvider>,
 	document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
